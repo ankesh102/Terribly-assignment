8435068cdc9ad6aa267ff485f0bb187557960127
ankesh102 <ankesh.pandey8@gmail.com>
2023-05-15 15:40:44 +0530

    first-commit

---
+++ src/components/submit/Submit.jsx
@@ -0,0 +1,12 @@
+import React from 'react' ;
+import './Submit.css' ;
+
+export default function Submit() {
+  return (
+    <div>
+      <div className="container">
+        <button id='btn-submit'>Submit</button>
+      </div>
+    </div>
+  )
+}
